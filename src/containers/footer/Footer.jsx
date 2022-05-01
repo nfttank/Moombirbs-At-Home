f6300c59bec8863c0d39bd651c101f3baab45aea
nfttank <nfttank>
2022-05-01 19:58:53 +0200

    Add the tweet of the bet

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import tank from '../../assets/4227.png';
+import the_bet from '../../assets/the_bet.png';
 import './footer.css';
 
 const Footer = (props) => (
     <div className="og__footer section__padding">
         <div className="og__footer-copyright">
-            <p>Moonbirds At Home are CC0 and with that in public domain. Feel free to use your tokens in any way you want.</p>
+            <a href="https://twitter.com/nfttank/status/1516757063308877824?s=20&t=NhQ8KdEZsn2yR1dBCRaA5w" target="_blank" rel="noopener noreferrer">
+                <img alt="Tanks bet to build a Moonbird collection with 1/150 of the original Moonbird mint." className="og__footer-tweet" src={the_bet} />
+            </a>
+            <p>Moonbirds At Home are CC0 and with that in public domain.<br />Feel free to use your tokens in any way you want.</p>
             <a href={props.data.tankTwitterUrl} target="_blank" rel="noopener noreferrer">
                 <img alt="profile of Tank showing CryptoPunk #4227" className="og__footer-avatar" src={tank} />
             </a>
@@ -15,3 +19,6 @@ const Footer = (props) => (
 );
 
 export default Footer;
+
+
+
